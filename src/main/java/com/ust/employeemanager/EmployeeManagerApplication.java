47b877566c32bfa5d683d58ac936a0d5085f302c
daltonhutt <103676585+daltonhutt@users.noreply.github.com>
2022-07-25 21:27:46 -0400

    Employee Manager App Back End

---
+++ src/main/java/com/ust/employeemanager/EmployeeManagerApplication.java
@@ -0,0 +1,36 @@
+package com.ust.employeemanager;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class EmployeeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagerApplication.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
+}
+
+
+
